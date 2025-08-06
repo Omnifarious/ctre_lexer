ff3cfca3f30837792f6c86293181fb7a2c6bb3dc
Eric Hopper <hopper@omnifarious.org>
2025-08-05 20:49:24 -0700

    Giant not switch statement activated to handle each token type.

--- lexer_test.cpp
+++ lexer_test.cpp
@@ -11,16 +11,35 @@
 #include <format>
 
 static constexpr auto lex_patterns = ctll::fixed_string{
-   "\\s*(?:"
+   "(?m)\\s*(?:"
    "(?<int_hex>0x[0-9a-fA-F]+)|"
    "(?<int_oct>0[0-7]*[1-7][0-7]*)|"
-   "(?<int_dec>(?:[1-9][0-9]*)|0)"
+   "(?<int_dec>(?:[1-9][0-9]*)|0)|"
+   "(?<identifier>\\w(?:\\w|\\d)*)"
    ")"
 };
 
 auto const tokenizer_re = ::ctre::tokenize<
    lex_patterns
 >;
+
+namespace Tokens {
+
+struct Base {
+   ::std::string orig_text_;
+};
+
+struct UnsignedInteger : Base {
+   ::std::uintmax_t value_;
+};
+
+struct Identifier : Base {
+   ::std::string value_;
+};
+
+using AnyToken = ::std::variant<UnsignedInteger, Identifier>;
+
+}
 
 template <::std::forward_iterator I>
 ::std::vector<::std::string> tokenize_tokenizer(I begin, I end)
@@ -36,7 +55,7 @@ template <::std::forward_iterator I>
    return result;
 }
 
-int main()
+void tokenize_test()
 {
    using rawchars_t = ::std::istreambuf_iterator<char>;
    auto tokenize = [](){
@@ -48,5 +67,36 @@ int main()
    auto const tokens = tokenize();
    for (auto const &token: tokens) {
       ::std::cout << ::std::format(" - [\"{}\"]\n", token);
+   }
+}
+
+int main()
+{
+   using rawchars_t = ::std::istreambuf_iterator<char>;
+   input_to_forward_range_adapter adapter{
+      rawchars_t{::std::cin}, rawchars_t{}
+   };
+   auto matches = tokenizer_re(adapter.begin(), adapter.end());
+   ::std::vector<Tokens::AnyToken> tokens;
+   using ::std::format;
+   using ::std::cout;
+   using ::std::stoull;
+   for (auto const &match: matches) {
+      if (match) {
+         ::std::clog << "got one" << ::std::endl;
+         ::std::clog << ::std::format(" - [\"{}\"]\n", match.to_string()) << ::std::flush;
+         if (auto const &dec = match.get<"int_dec">()) {
+            auto const num = dec.to_string();
+            tokens.emplace_back(Tokens::UnsignedInteger{num, stoull(num, nullptr, 10)});
+         }
+         if (auto const &hex = match.get<"int_hex">()) {
+            auto const num = hex.to_string();
+            tokens.emplace_back(Tokens::UnsignedInteger{num, stoull(num, nullptr, 16)});
+         }
+         if (auto const &oct = match.get<"int_oct">()) {
+            auto const num = oct.to_string();
+            tokens.emplace_back(Tokens::UnsignedInteger{num, stoull(num, nullptr, 8)});
+         }
+      }
    }
 }
