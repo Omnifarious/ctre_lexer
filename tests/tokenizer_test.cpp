dc9ea4ae29cbd5c79d422171bbad51b09ec29ad5
Eric Hopper <hopper@omnifarious.org>
2025-08-17 21:42:57 -0700

    Start adding unit tests for main program as prelude to refactoring.

---
+++ tests/tokenizer_test.cpp
@@ -0,0 +1,13 @@
+// Copyright 2025 by Eric Hopper
+// See project LICENSE file for details
+
+#define main temporarily_rename_this_function
+#include "../lexer_test.cpp"
+#undef main
+
+
+#include <catch2/catch_all.hpp>
+
+SCENARIO("Fixed string generates expected list of tokens.")
+{
+}
